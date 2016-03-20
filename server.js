e5b5b97b13498ebb6945507b6cf9039896add1c9
Mark Mnei <MarkMnei@gmail.com>
2016-03-20 19:08:29 -0400

    added todos and todos/:id routes

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+},{
+    id: 2,
+    description: 'Go to Market',
+    completed: false
+},{
+    id: 3,
+    description: 'Go to School',
+    completed: true
+}];
 
 //GET Request
 app.get('/', function(req, res) {
-  res.send("Todo API Root");
+  res.json("Todo API Root");
+});
+
+//GET /todos
+app.get('/todos', function(req, res){
+
+
+    res.json(todos);
+});
+
+//GET /todo/:id
+app.get('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = null;
+    
+    todos.forEach(function (todo) {
+        if(todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+    if(matchedTodo === undefined) {
+        res.status(404).send({ error: "boo:(" });
+    } else {
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function() {
