62129331715ce792265ec201d27a92c2ee23690a
Mark Mnei <MarkMnei@gmail.com>
2016-03-26 11:39:50 -0400

    Added ?completed filter

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(bodyParser.json());
 
 //GET /todos
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')  {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+    res.json(filteredTodos);
 });
 
 //GET /todos/:id
@@ -34,7 +42,6 @@ app.post('/todos', function(req, res) {
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
         return res.status(400).send();
     }
-
     body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
